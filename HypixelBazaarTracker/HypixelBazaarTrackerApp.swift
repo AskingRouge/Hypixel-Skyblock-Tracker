563b1018041c06c57ba4f0b66e509f431e33cc97
Nikhil B <nikhilrbudamagunta@gmail.com>
2026-04-21 16:01:59 -0400

    Initial Commit

---
+++ HypixelBazaarTracker/HypixelBazaarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypixelBazaarTrackerApp.swift
+//  HypixelBazaarTracker
+//
+//  Created by Nikhil Budamagunta on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HypixelBazaarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
